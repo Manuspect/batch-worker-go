257aea97ba592ea792c6764ff6b0d44fda92abad
Михаил <mihnikm@gmail.com>
2024-07-23 01:04:46 +0300

    parse .csv file for send to ...

--- internal/batch/get_csv.go
+++ internal/batch/get_csv.go
@@ -7,18 +7,18 @@ import (
 )
 
 type FileCsv struct {
-	Timestamp     string `csv:"timestamp"`
-	Process_path  string `csv:"process_path"`
-	Title         string `csv:"title"`
-	Class_name    string `csv:"class_name"`
-	Window_left   string `csv:"window_left"`
-	Window_top    string `csv:"window_top"`
-	Window_right  string `csv:"window_right"`
-	Window_bottom string `csv:"window_bottom"`
-	Event         string `csv:"event"`
-	Mouse_x_pos   string `csv:"mouse_x_pos"`
-	Mouse_y_pos   string `csv:"mouse_y_pos"`
-	Modifiers     string `csv:"modifiers"`
+	Timestamp     string `json:"timestamp"`
+	Process_path  string `json:"process_path"`
+	Title         string `json:"title"`
+	Class_name    string `json:"class_name"`
+	Window_left   string `json:"window_left"`
+	Window_top    string `json:"window_top"`
+	Window_right  string `json:"window_right"`
+	Window_bottom string `json:"window_bottom"`
+	Event         string `json:"event"`
+	Mouse_x_pos   string `json:"mouse_x_pos"`
+	Mouse_y_pos   string `json:"mouse_y_pos"`
+	Modifiers     string `json:"modifiers"`
 }
 
 func GetEvetsCsv(pathCsv string) ([]FileCsv, error) {
@@ -34,20 +34,51 @@ func GetEvetsCsv(pathCsv string) ([]FileCsv, error) {
 	for i := 0; scanner.Scan(); i++ {
 		line := scanner.Text()
 
-		lineArr := strings.Split(line, ",")
+		lineArr2 := strings.Split(line, ",")
+		var lineArr = [12]string{}
+
+		if len(lineArr2) == 12 {
+			lineArr[0] = lineArr2[0]
+			lineArr[1] = lineArr2[1]
+			lineArr[2] = lineArr2[2]
+			lineArr[3] = lineArr2[3]
+			lineArr[4] = lineArr2[4]
+			lineArr[5] = lineArr2[5]
+			lineArr[6] = lineArr2[6]
+			lineArr[7] = lineArr2[7]
+			lineArr[8] = lineArr2[8]
+			lineArr[9] = lineArr2[9]
+			lineArr[10] = lineArr2[10]
+			lineArr[11] = lineArr2[11]
+		}
+
+		if len(lineArr2) == 13 {
+			lineArr[0] = lineArr2[0]
+			lineArr[1] = lineArr2[1]
+			lineArr[2] = lineArr2[2]
+			lineArr[3] = lineArr2[3]
+			lineArr[4] = lineArr2[4]
+			lineArr[5] = lineArr2[5]
+			lineArr[6] = lineArr2[6]
+			lineArr[7] = lineArr2[7]
+			lineArr[8] = lineArr2[8]
+			lineArr[9] = lineArr2[9]
+			lineArr[10] = lineArr2[10]
+			lineArr[11] = lineArr2[11] + ", " + lineArr2[12]
+		}
 
 		events = append(events, FileCsv{
-			Timestamp:     lineArr[0],
-			Process_path:  lineArr[1],
-			Title:         lineArr[2],
-			Class_name:    lineArr[3],
-			Window_left:   lineArr[4],
-			Window_top:    lineArr[5],
-			Window_right:  lineArr[6],
-			Window_bottom: lineArr[7],
-			Event:         lineArr[8],
-			Mouse_x_pos:   lineArr[9],
-			Mouse_y_pos:   lineArr[10],
+			Timestamp:     lineArr[0] + "|",
+			Process_path:  lineArr[1] + "|",
+			Title:         lineArr[2] + "|",
+			Class_name:    lineArr[3] + "|",
+			Window_left:   lineArr[4] + "|",
+			Window_top:    lineArr[5] + "|",
+			Window_right:  lineArr[6] + "|",
+			Window_bottom: lineArr[7] + "|",
+			Event:         lineArr[8] + "|",
+			Mouse_x_pos:   lineArr[9] + "|",
+			Mouse_y_pos:   lineArr[10] + "|",
 			Modifiers:     lineArr[11],
 		})
 
--- internal/queue/nats.go
+++ internal/queue/nats.go
@@ -1,14 +1,18 @@
 package nats
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"processing-worker/internal/batch"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/nats-io/nats.go"
@@ -115,31 +119,102 @@ func CreateConsumerHandler(m *minio.Client, fc jetstream.Consumer) func(jetstrea
 			fmt.Println("events: OK")
 		}
 
+		type Info struct {
+			Timestamp     string `json:"timestamp"`
+			Process_path  string `json:"process_path"`
+			Title         string `json:"title"`
+			Class_name    string `json:"class_name"`
+			Window_left   string `json:"window_left"`
+			Window_top    string `json:"window_top"`
+			Window_right  string `json:"window_right"`
+			Window_bottom string `json:"window_bottom"`
+			Event         string `json:"event"`
+			Mouse_x_pos   string `json:"mouse_x_pos"`
+			Mouse_y_pos   string `json:"mouse_y_pos"`
+			Modifiers     string `json:"modifiers"`
+			FrameName     string `json:"frameName"`
+		}
+
 		arr := strings.Split(objectName, "-")
 		contentType := "application/octet-image"
+		var frame_name string
 
 		for j, event := range events {
 
 			if j > 0 {
 
-				arr_timestamp := strings.Split(fmt.Sprintf("%v", event), " ")
+				arr_csv := strings.Split(fmt.Sprintf("%v", event), "|")
 
-				objectName = arr[0] + "-" + arr[1] + "-" + arr_timestamp[0][1:] + "-" + strconv.Itoa(j) + ".jpeg" // 2-3-44-3.jpeg
+				frame_name = arr[0] + "-" + arr[1] + "-" + arr_csv[0][1:] + "-" + strconv.Itoa(j) + ".jpeg" // 2-3-44-3.jpeg
 				filePath := "frames/" + strconv.Itoa(j) + ".jpeg"
 
 				_, err := m.FPutObject(
 					ctx,
 					bucketName,
-					objectName,
+					frame_name,
 					filePath,
 					minio.PutObjectOptions{ContentType: contentType})
 				if err != nil {
 					log.Println(err)
 				}
+
+				fileInfo := Info{
+					Timestamp:     arr_csv[0][1:],
+					Process_path:  arr_csv[1],
+					Title:         arr_csv[2],
+					Class_name:    arr_csv[3],
+					Window_left:   arr_csv[4],
+					Window_top:    arr_csv[5],
+					Window_right:  arr_csv[6],
+					Window_bottom: arr_csv[7],
+					Event:         arr_csv[8],
+					Mouse_x_pos:   arr_csv[9],
+					Mouse_y_pos:   arr_csv[10],
+					Modifiers:     arr_csv[11][0 : len(arr_csv[11])-1],
+					FrameName:     frame_name,
+				}
+
+				body, err := json.Marshal(fileInfo)
+				if err != nil {
+					log.Println(err)
+				}
+
+				fmt.Println("fileInfo:", fileInfo)
+
+				bodyReader := bytes.NewReader(body)
+
+				url := "http://localhost:3000/info" // TODO
+				req, err := http.NewRequest("Get", url, bodyReader)
+				if err != nil {
+					log.Println(err)
+				}
+
+				req.Header.Set("Content-Type", "application/json")
+
+				client := http.Client{
+					Timeout: 5 * time.Second,
+				}
+
+				resp, err := client.Do(req)
+				if err != nil {
+					log.Println(err)
+				}
+
+				b, err := io.ReadAll(resp.Body)
+				if err != nil {
+					log.Println(err)
+				}
+				fmt.Println(string(b))
+
 			}
 		}
 
 		err = os.RemoveAll("frames")
+		if err != nil {
+			log.Println(err)
+		}
+
+		err = os.RemoveAll(objectName)
 		if err != nil {
 			log.Println(err)
 		}
